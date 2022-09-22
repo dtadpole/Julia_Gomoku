eccbfbd71e73a37696b707804bce7813d4ddf159
D.Tadpole <gsk390@gmail.com>
2022-09-21 23:14:02 -0700

    info

--- infer.jl
+++ infer.jl
@@ -3,51 +3,55 @@ include("./mcts.jl")
 using JSON
 
 """infer playouts"""
-function infer_playouts()
+function infer_playout(i::Int)
 
     global URL_BASE
 
-    model_1 = nothing
-    model_2 = nothing
+    @info repeat("-", 50)
+    @info "Inference Iteration [$(i)] ..."
+    @info repeat("-", 50)
+
+    url = "$(URL_BASE)/match"
+
+    r = HTTP.request(:GET, "$(url)")
+    if r.status != 200
+        @warn "Requested match [$(url)] : [status = $(r.status)]"
+        exit(1)
+    end
+
+    io = IOBuffer(r.body)
+    id_1, id_2 = JSON.parse(io)
+    @info "Requested match [$(url)] : [status = $(r.status)] [$(id_1) vs $(id_2)]"
+
+    model_1 = download_model(id_1)
+    model_2 = download_model(id_2)
+
+    @info repeat("-", 50)
+
+    experiences, game_init_turn, game_score = mcts_play_game(model_1, model_2)
+
+    id_first, id_second = game_init_turn > 0 ? (id_1, id_2) : (id_2, id_1)
+
+    normalized_score = game_init_turn > 0 ? game_score : -game_score
+
+    io = IOBuffer()
+    serialize(io, (id_first, id_second, normalized_score, experiences))
+
+    url = "$(URL_BASE)/game"
+    @info "Posting game [$(url)] [$(id_first) vs $(id_second)] [$(normalized_score)] [len = $(length(experiences))]"
+    r = HTTP.request(:POST, url, body=take!(io))
+    @info "Posted experiences [$(url)] [status = $(r.status)]"
+
+
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
 
     for i in 1:args["infer_playouts"]
-
         try
 
-            @info repeat("-", 50)
-            @info "Inference Iteration [$(i)] ..."
-            @info repeat("-", 50)
-
-            url = "$(URL_BASE)/match"
-
-            r = HTTP.request(:GET, "$(url)")
-            if r.status != 200
-                @warn "Requested match [$(url)] : [status = $(r.status)]"
-                exit(1)
-            end
-
-            io = IOBuffer(r.body)
-            id_1, id_2 = JSON.parse(io)
-            @info "Requested match [$(url)] : [status = $(r.status)] [$(id_1) vs $(id_2)]"
-
-            model_1 = download_model(id_1)
-            model_2 = download_model(id_2)
-
-            @info repeat("-", 50)
-
-            experiences, game_init_turn, game_score = mcts_play_game(model_1, model_2)
-
-            id_first, id_second = game_init_turn > 0 ? (id_1, id_2) : (id_2, id_1)
-
-            normalized_score = game_init_turn > 0 ? game_score : -game_score
-
-            io = IOBuffer()
-            serialize(io, (id_first, id_second, normalized_score, experiences))
-
-            url = "$(URL_BASE)/game"
-            @info "Posting game [$(url)] [$(id_first) vs $(id_second)] [$(normalized_score)] [len = $(length(experiences))]"
-            r = HTTP.request(:POST, url, body=take!(io))
-            @info "Posted experiences [$(url)] [status = $(r.status)]"
+            infer_playout(i)
 
         catch e
 
@@ -75,8 +79,4 @@ function infer_playouts()
     @info "Inference Completed [$(args["infer_playouts"])] Iterations ."
     @info repeat("-", 50)
 
-end
-
-if abspath(PROGRAM_FILE) == @__FILE__
-    infer_playouts()
 end
