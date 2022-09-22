d9e0224ccbcef7ca24fafe37e00726fca3efca20
D.Tadpole <gsk390@gmail.com>
2022-09-22 02:52:47 -0700

    args

--- args.jl
+++ args.jl
@@ -200,7 +200,7 @@ function parse_commandline()
         "--elo_below_avg_cutoff"
         help = "elo below average cutoff"
         arg_type = Int
-        default = 120
+        default = 100
 
         "--elo_k_value"
         help = "elo k value"
