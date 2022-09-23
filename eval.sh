904161c6331c62174a078bc6a7508e6aeb5504c9
D.Tadpole <gsk390@gmail.com>
2022-09-23 12:39:20 -0700

    eval

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true ; do
-    julia ./eval.jl --mcts_temperature_mean 0.25 "$@"
+    julia ./eval.jl --mcts_temperature_mean 0.2 --mcts_temperature_mean 0.1 "$@"
     sleep 30
 done
