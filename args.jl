ee0d7e3e26673c8481308d74d51e75c9e8b8bd13
D.Tadpole <gsk390@gmail.com>
2022-09-22 23:13:41 -0700

    best 11x11

--- args.jl
+++ args.jl
@@ -89,7 +89,7 @@ function parse_commandline()
         "--mcts_temperature_mean"
         help = "mcts temperature mean"
         arg_type = Float32
-        default = Float32(0.9)
+        default = Float32(0.85)
 
         "--mcts_temperature_std"
         help = "mcts temperature std"
